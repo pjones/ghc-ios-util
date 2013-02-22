f202ef7f080e0e73cef08dc149fcce31bdf4c545
Peter Jones <pjones@pmade.com>
2013-02-22 16:12:02 -0700

    Rework the checkout script to document the required branches/commits that each of sub-repositories needs to be on

--- ghc/checkout.sh
+++ ghc/checkout.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+################################################################################
 # For each git repository in the current directory (including the
 # current directory) switch to the ios branch or another appropriate
 # branch.
 
+################################################################################
 # Nice way to die.
 die () {
   echo "ERROR: " "$@" > /dev/stderr
   exit 1
 }
 
+################################################################################
 # Checkout the given branch.
 checkout () {
   branch=$1
@@ -19,29 +22,23 @@ checkout () {
   echo # newline
 }
 
-# Some of the repositories need to be on the ios branch.
-for sb in `find . -type d -name .git`; do
-  base=`dirname $sb`
+################################################################################
+# These repositories need to be on specific commits/branches.
+(cd libraries/array     && checkout 81df1ffbd3d1ebbbf5eedf9ce7ee770cec93773c)
+(cd libraries/directory && checkout remotes/origin/ghc-7.4)
+(cd libraries/ghc-prim  && checkout 529167773a9a036d7d3262707be4c5a2f6a04bcf)
+(cd libraries/haskeline && checkout 38d57bef2807443b84680f78a9bdb6c59c907192)
+(cd libraries/hoopl     && checkout remotes/origin/ghc-7.4)
+(cd libraries/process   && checkout remotes/origin/ghc-7.4)
 
+################################################################################
+# If a repository has an "ios" branch, use that instead.
+for sb in `find . -type d -name .git`; do
   ( # New sub-shell to avoid having to cd back to where we were.
-    cd $base
+    cd `dirname $sb`
 
     if git branch -a 2>&1 | grep -q '/ios'; then
       checkout ios && git pull -q
     fi
   )
-done
-
-# The name of the 7.4 release branch.
-relbranch=remotes/origin/ghc-7.4
-
-# Repositories that need to be on the $relbranch.
-dirs="
-  libraries/directory
-  libraries/hoopl
-  libraries/process
-"
-
-for d in $dirs; do
-  (cd $d && checkout $relbranch)
 done
